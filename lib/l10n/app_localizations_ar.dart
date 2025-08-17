4a545b314ee71bdc879dd003e0c187f6a54ed881
Bassant <bassantsalama22@gmail.com>
2025-08-18 01:40:59 +0300

    Inilialize Movies App

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,91 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get watch => 'مشاهدة';
+
+  @override
+  String get exploreNow => 'Explore Now';
+
+  @override
+  String get next => 'التالي';
+
+  @override
+  String get back => 'السابق';
+
+  @override
+  String get screenShots => 'Screen Shots';
+
+  @override
+  String get similar => 'مشابه';
+
+  @override
+  String get summary => 'الملخص';
+
+  @override
+  String get cast => 'طاقم العمل';
+
+  @override
+  String get genres => 'الأنواع';
+
+  @override
+  String get email => 'البريد الإلكتروني';
+
+  @override
+  String get password => 'كلمة المرور';
+
+  @override
+  String get forgetPasswordQuestion => 'هل نسيت كلمة المرور؟';
+
+  @override
+  String get forgetPassword => 'Forget Password';
+
+  @override
+  String get or => 'أو';
+
+  @override
+  String get loginWithGoogle => 'Login With Google';
+
+  @override
+  String get name => 'الاسم';
+
+  @override
+  String get confirmPassword => 'Confirm Password';
+
+  @override
+  String get phoneNumber => 'Phone Number';
+
+  @override
+  String get createAccount => 'Create Account';
+
+  @override
+  String get alreadyHaveAccount => 'Already Have Account ?';
+
+  @override
+  String get login => 'تسجيل الدخول';
+
+  @override
+  String get register => 'تسجيل';
+
+  @override
+  String get verifyEmail => 'Verify Email';
+
+  @override
+  String get wishList => 'Wish List';
+
+  @override
+  String get history => 'السجل';
+
+  @override
+  String get deleteAccount => 'Delete Account';
+
+  @override
+  String get updateData => 'Update Data';
+}
