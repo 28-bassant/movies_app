9c275c92339d2255c90b1975a9b8b7a36807b0ff
Bassant <bassantsalama22@gmail.com>
2025-08-18 01:40:59 +0300

    Initialize Music App

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,91 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get watch => 'Watch';
+
+  @override
+  String get exploreNow => 'Explore Now';
+
+  @override
+  String get next => 'Next';
+
+  @override
+  String get back => 'Back';
+
+  @override
+  String get screenShots => 'Screen Shots';
+
+  @override
+  String get similar => 'Similar';
+
+  @override
+  String get summary => 'Summary';
+
+  @override
+  String get cast => 'Cast';
+
+  @override
+  String get genres => 'Genres';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get forgetPasswordQuestion => 'Forget Password ?';
+
+  @override
+  String get forgetPassword => 'Forget Password';
+
+  @override
+  String get or => 'OR';
+
+  @override
+  String get loginWithGoogle => 'Login With Google';
+
+  @override
+  String get name => 'Name';
+
+  @override
+  String get confirmPassword => 'Confirm Password';
+
+  @override
+  String get phoneNumber => 'Phone Number';
+
+  @override
+  String get createAccount => 'Create Account';
+
+  @override
+  String get alreadyHaveAccount => 'Already Have Account ?';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get register => 'Register';
+
+  @override
+  String get verifyEmail => 'Verify Email';
+
+  @override
+  String get wishList => 'Wish List';
+
+  @override
+  String get history => 'History';
+
+  @override
+  String get deleteAccount => 'Delete Account';
+
+  @override
+  String get updateData => 'Update Data';
+}
