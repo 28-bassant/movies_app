b83e6734f7c50eb203df226974d4e104b57efa38
Bassant <bassantsalama22@gmail.com>
2025-08-20 12:46:23 +0300

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/UI/auth/forget_password/forget_password_screen.dart';
 import 'package:movies_app/UI/auth/login/login-screen.dart';
-import 'package:movies_app/UI/auth/on_boarding/on_boarding_screen.dart';
+import 'package:movies_app/UI/on_boarding/on_boarding_screen.dart';
 import 'package:movies_app/UI/auth/update/update_screen.dart';
 import 'package:movies_app/providers/app-language-provider.dart';
 import 'package:movies_app/utils/app_routes.dart';
